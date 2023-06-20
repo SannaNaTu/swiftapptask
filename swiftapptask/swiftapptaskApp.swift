f4205f864f3b6ad21324363af5954be472d80c37
korhonsm@gmail.com <korhonsm@gmail.com>
2023-06-20 21:36:07 +0200

    Initial Commit

---
+++ swiftapptask/swiftapptaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftapptaskApp.swift
+//  swiftapptask
+//
+//  Created by user242973 on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftapptaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
